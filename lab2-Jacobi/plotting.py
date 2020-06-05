bb44c09825434d14232b5e9d602f0ec37d0940f6
Andrey Chausov <dxahtepb@gmail.com>
2020-06-05 12:29:57 +0300

    [lab-2] Update pictures legend

--- lab2-Jacobi/plotting.py
+++ lab2-Jacobi/plotting.py
@@ -10,14 +10,14 @@ def read_json(filename):
 
 
 def plot():
-    parent_dir = 'runs/test_runs/'
+    parent_dir = 'runs/bcast_runs/'
     filenames = os.listdir(parent_dir)
 
     run_times_for_sizes = {}
     for filename in filenames:
         run_results = read_json(parent_dir + filename)
         for run_result in run_results['runs']:
-            thread_num = f'{run_result["thread_num"]} thread(s)'
+            thread_num = f'{run_result["thread_num"]} cores(s)'
             if thread_num not in run_times_for_sizes:
                 run_times_for_sizes[thread_num] = []
             run_times_for_sizes[thread_num].append((
@@ -33,7 +33,7 @@ def plot():
 
     for line_label, runtimes_for_size in sorted(run_times_for_sizes.items()):
         sizes = [f'{x[0]}' for x in runtimes_for_size]
-        speedups = [run_times_for_sizes['1 thread(s)'][i][1] / x[1] for i, x in enumerate(runtimes_for_size)]
+        speedups = [run_times_for_sizes['1 cores(s)'][i][1] / x[1] for i, x in enumerate(runtimes_for_size)]
         ax.plot(sizes, speedups, label=line_label)
 
     plt.ylabel('Speedup, times')
@@ -41,7 +41,7 @@ def plot():
     lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, ncol=2)
 
     os.makedirs('./images/', exist_ok=True)
-    plt.savefig('./images/jacobi_2.png', bbox_extra_artists=(lgd, ), bbox_inches='tight')
+    plt.savefig('./images/jacobi_1.png', bbox_extra_artists=(lgd, ), bbox_inches='tight')
 
 
 def main():
