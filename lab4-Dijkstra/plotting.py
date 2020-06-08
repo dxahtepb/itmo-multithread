aa8550b53765b14f07ec78284f2c043985c1a1c1
Andrey Chausov <dxahtepb@gmail.com>
2020-06-08 23:27:29 +0300

    [lab-4] Introduce benchmarks

---
+++ lab4-Dijkstra/plotting.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+import matplotlib.pyplot as plt
+
+
+def read_json(filename):
+    with open(filename) as file:
+        return json.load(file)
+
+
+def plot(name):
+    parent_dir = f'runs/{name}/'
+    filenames = os.listdir(parent_dir)
+
+    run_times_for_sizes = {}
+    for filename in filenames:
+        run_results = read_json(parent_dir + filename)
+        for run_result in run_results['runs']:
+            thread_num = run_result["thread_num"]
+            if thread_num not in run_times_for_sizes:
+                run_times_for_sizes[thread_num] = []
+            run_times_for_sizes[thread_num].append((
+                run_result['size'],
+                run_result['run_time']
+            ))
+
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.add_subplot(111)
+
+    for line_label in run_times_for_sizes:
+        run_times_for_sizes[line_label] = sorted(run_times_for_sizes[line_label])
+
+    for line_label, runtimes_for_size in sorted(run_times_for_sizes.items()):
+        sizes = [f'{x[0]}' for x in runtimes_for_size]
+        speedups = [run_times_for_sizes[1][i][1] / x[1] for i, x in enumerate(runtimes_for_size)]
+        ax.plot(sizes, speedups, label=f'{line_label} core(s)')
+
+    plt.ylabel('Speedup, times')
+    plt.xlabel('Sizes')
+    lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, ncol=2)
+
+    os.makedirs('./images/', exist_ok=True)
+    plt.savefig(f'./images/{name}.png', bbox_extra_artists=(lgd, ), bbox_inches='tight')
+
+
+def main():
+    plot('static')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab4-Dijkstra/run.py
@@ -0,0 +1,97 @@
+import copy
+import json
+import os
+import random
+import subprocess
+from datetime import datetime
+
+
+def generate_full_graph(size):
+    lst = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        for j in range(i + 1, size):
+            lst[j][i] = lst[i][j] = random.randint(1, 1000)
+    return lst
+
+
+def write_matrix(path, matrix):
+    with open(path, 'w') as output:
+        output.write(f'{len(matrix)}\n')
+        for row in matrix:
+            output.write(' '.join(map(str, row)) + '\n')
+
+
+def run_tests():
+    executable = './cmake-build-debug/lab4'
+
+    run_results = {
+        'executable': 'omp',
+        'runs': []
+    }
+
+    # for size in (10, 100, 300, 500, 800, 1_000, 2_000, 4_000, 5_000, 10_000, 20_000):
+    for size in ( 5_000,):
+        matrix_path = f'./matrices/{size}_matrix.txt'
+        if not os.path.exists(matrix_path):
+            write_matrix(matrix_path, generate_full_graph(size))
+
+        for thread_num in (1, 2, 4, 6, 8, 12):
+            output = f'./matrices/{size}_{thread_num}_result.txt'
+            start_element = 0
+
+            executable_named_params = {
+                'run_mode': 'parallel',
+                'run_times': 3,
+                'num_threads': thread_num,
+                'schedule': 'static',
+                'chunk_size': -1,
+            }
+
+            options_list = make_options_list(executable_named_params)
+
+            try:
+                proc = subprocess.Popen([executable, matrix_path, str(start_element),
+                                         output, '--options', *options_list],
+                                        stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+                out, err = proc.communicate()
+                print(f'{size} {thread_num}: {str(out.strip(), encoding="utf-8")}')
+                print(f'stderr: {str(err, encoding="utf-8")}')
+                run_time = int(out)
+
+                run_results['runs'].append({
+                    'thread_num': thread_num,
+                    'size': size,
+                    'run_time': run_time,
+                    'named_params': copy.deepcopy(executable_named_params)
+                })
+
+            except subprocess.CalledProcessError as err:
+                print(f'process finished with error: {err}')
+
+    return run_results
+
+
+def make_options_list(options):
+    options_list = []
+    for item in options.items():
+        options_list.append(str(item[0]))
+        options_list.append(str(item[1]))
+    return options_list
+
+
+def write_run_results(run_results, sub_folder='misc'):
+    try:
+        os.makedirs(f'./runs/{sub_folder}', exist_ok=True)
+        with open(f'./runs/{sub_folder}/run_{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}', 'w') as file:
+            json.dump(run_results, file, indent=2)
+    except IOError as err:
+        print(f'Can not write results: {err}')
+
+
+def main():
+    # run_tests()
+    write_run_results(run_tests(), 'test_runs')
+
+
+if __name__ == '__main__':
+    main()
